be6154cf6bd2b8191b3d4701bff3fdb46ee0d3bc
manelmourelo <manelmourelo.1@gmail.com>
2020-06-02 20:34:36 +0200

    Vertical Blur in the correct attachment

--- Engine/Project3/src/rendering/deferredrenderer.cpp
+++ Engine/Project3/src/rendering/deferredrenderer.cpp
@@ -639,11 +639,11 @@ void DeferredRenderer::passBloom(){
     passBlur(fboBloom5, QVector2D(w/32,h/32), GL_COLOR_ATTACHMENT1, rtBright, LOD(4), horizontal);
 
     //Vertical Blur
-    passBlur(fboBloom1, QVector2D(w/2,h/2), GL_COLOR_ATTACHMENT1, rtBloomH, LOD(0), vertical);
-    passBlur(fboBloom2, QVector2D(w/4,h/4), GL_COLOR_ATTACHMENT1, rtBloomH, LOD(1), vertical);
-    passBlur(fboBloom3, QVector2D(w/8,h/8), GL_COLOR_ATTACHMENT1, rtBloomH, LOD(2), vertical);
-    passBlur(fboBloom4, QVector2D(w/16,h/16), GL_COLOR_ATTACHMENT1, rtBloomH, LOD(3), vertical);
-    passBlur(fboBloom5, QVector2D(w/32,h/32), GL_COLOR_ATTACHMENT1, rtBloomH, LOD(4), vertical);
+    passBlur(fboBloom1, QVector2D(w/2,h/2), GL_COLOR_ATTACHMENT0, rtBloomH, LOD(0), vertical);
+    passBlur(fboBloom2, QVector2D(w/4,h/4), GL_COLOR_ATTACHMENT0, rtBloomH, LOD(1), vertical);
+    passBlur(fboBloom3, QVector2D(w/8,h/8), GL_COLOR_ATTACHMENT0, rtBloomH, LOD(2), vertical);
+    passBlur(fboBloom4, QVector2D(w/16,h/16), GL_COLOR_ATTACHMENT0, rtBloomH, LOD(3), vertical);
+    passBlur(fboBloom5, QVector2D(w/32,h/32), GL_COLOR_ATTACHMENT0, rtBloomH, LOD(4), vertical);
 
     passBloom2(fbo, GL_COLOR_ATTACHMENT3, rtBright, 4);
 
